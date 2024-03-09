f5946cda7c5b5c8b1eb831a347ed785318ca2f35
IshratJahan2024 <160525938+IshratJahan2024@users.noreply.github.com>
2024-03-09 22:22:52 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+console.log("Hello,World!");
+console.log("My Class");
+console.log("Governer of initiative class");
+console.log("My classes start from 15th of feb");
+console.log("Hello apna college");
+console.log("This is my first IT class");
+
+
+
+
